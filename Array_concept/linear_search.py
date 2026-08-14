bf1a6aa84533ea3ca4e9f9929fef56109671a937
vags2105-tec <mdvagdevi@gmail.com>
2026-08-14 16:43:33 +0530

    Linear search

--- Array_concept/linear_search.py
+++ Array_concept/linear_search.py
@@ -5,3 +5,9 @@ target=6
 for i in range(len(a)):
     if a[i]==target:
         print("Found at index",i)
+        break
+
+
+    #why we use break statement here? 
+    # that is beacuse once target is found
+    #  no need of continuing the loop just stop the loop here .

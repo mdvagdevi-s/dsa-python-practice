7c760e58e1f1f932c5f2e927dd66f1d020940fd2
vags2105-tec <mdvagdevi@gmail.com>
2026-08-15 11:05:50 +0530

    Updated Binary Search

--- Array_concept/binary_search.py
+++ Array_concept/binary_search.py
@@ -1,15 +1,19 @@
 a= [2, 5, 8, 12, 16, 20, 25]
-target = 16
+target = 100
 low=0
 high=len(a)-1
+found=False
 
 while low<=high:
     mid=(low+high)//2
     if a[mid]==target:
         print("Found at index",mid)
+        found=True
         break
     elif a[mid]<target:
         low=mid+1
 
     else:
         high=mid-1
+if not found:
+    print("Not found!")

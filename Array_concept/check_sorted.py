b2f99406b81bde496ae80702593316d1f9a1f392
vags2105-tec <mdvagdevi@gmail.com>
2026-08-16 10:44:39 +0530

    Checking yhe array is sorted

---
+++ Array_concept/check_sorted.py
@@ -0,0 +1,12 @@
+a = [10, 20,15, 40, 50]
+is_sorted=True
+for i in range(0,len(a)-1):
+    if a[i]>a[i+1]:
+        is_sorted=False
+        
+        break
+
+if is_sorted :
+    print("Array is sorted")
+else:
+    print("Array is not sorted")

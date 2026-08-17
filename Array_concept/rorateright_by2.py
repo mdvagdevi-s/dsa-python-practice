135d98799352eb1724331acfddd6e7edb229ec44
vags2105-tec <mdvagdevi@gmail.com>
2026-08-17 17:29:17 +0530

    Rotating elements concept

--- Array_concept/rorateright_by2.py
+++ Array_concept/rorateright_by2.py
@@ -6,11 +6,11 @@ def reverse(a,l,r):
         a[l],a[r]=a[r],a[l]
         l+=1
         r-=1
+reverse(a,0,len(a)-1)
+
 reverse(a,0,k-1)
 
 reverse(a,k,len(a)-1)
-
-reverse(a,0,len(a)-1)
 
 print(a)
 

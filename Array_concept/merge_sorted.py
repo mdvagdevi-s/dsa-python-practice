70021807e700460b77b37f7a53bb48a43dc9d87d
vags2105-tec <mdvagdevi@gmail.com>
2026-08-17 18:08:38 +0530

    Merging two sorting array

--- Array_concept/merge_sorted.py
+++ Array_concept/merge_sorted.py
@@ -10,4 +10,12 @@ while i<len(a) and j<len(b):
     else:
         res.append(b[j])
         j+=1
+while i < len(a):
+    res.append(a[i])
+    i += 1
+
+while j < len(b):
+    res.append(b[j])
+    j += 1
+
 print(res)

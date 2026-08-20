7799d696b0bcf2cdd1b7224bab131ea0c30adda9
vags2105-tec <mdvagdevi@gmail.com>
2026-08-20 18:01:51 +0530

    Two sum using hashing technique

---
+++ Array_concept/two sum2.py
@@ -0,0 +1,13 @@
+a=[2,7,11,15]
+target=9
+seen={}
+
+for i in range(len(a)):
+    current=a[i]
+    needed=target-current
+
+    if needed in seen:
+        print("Found in the index:",seen[needed],i)
+
+    else:
+        seen[current]=i
